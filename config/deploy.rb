bdc73499cfd8204edcc96a7358688a1834c8bcef
Alexandre Bini <alexandrebini@gmail.com>
2014-01-12 17:50:17 -0200

    versao certa do ruby

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :format, :pretty
 set :log_level, :debug
 set :pty, true
 
-set :bundle_flags, '--deployment --quiet --binstubs'
+# set :bundle_flags, '--deployment --quiet --binstubs'
 
 # set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
@@ -18,6 +18,20 @@ set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :keep_releases, 5
 
 namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
 
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
   after :finishing, 'deploy:cleanup'
 end
